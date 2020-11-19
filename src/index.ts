ec5a24c1d8cfbd15670a5c183e04d6c7bd18cb61
Géry Debongnie <ged@odoo.com>
2020-11-19 11:53:26 +0100

    add index.ts, remove old cruft

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * This file is the main file packaged by rollup (see rollup.config.js).  From
+ * this file, we export all public owl elements.
+ *
+ * Note that dynamic values, such as a date or a commit hash are added by rollup
+ */
+
+export { Component, mount, xml } from "./core";
+
+export const __info__ = {};
