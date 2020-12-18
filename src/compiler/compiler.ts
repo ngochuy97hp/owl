d279dcff6f817ec8ff5e1d56c0b07760641552fb
Géry Debongnie <ged@odoo.com>
2020-12-18 14:15:53 +0100

    wip

--- src/compiler/compiler.ts
+++ src/compiler/compiler.ts
@@ -43,6 +43,7 @@ export type TemplateFunction = (blocks: any, utils: any) => Template;
 
 interface FunctionLine {
   path: string[];
+  elemDef?: string;
   inserter(el: string): string;
 }
 
@@ -330,29 +331,37 @@ export class QWebCompiler {
     // build tree of paths
     const tree: any = {};
     let i = 1;
+    // console.warn('lines before', lines)
     for (let line of lines) {
       let current: any = tree;
       let el: string = `this`;
       for (let p of line.path.slice()) {
         if (current[p]) {
         } else {
-          current[p] = { firstChild: null, nextSibling: null };
+          current[p] = { firstChild: null, nextSibling: null, line };
         }
         if (current.firstChild && current.nextSibling && !current.name) {
           current.name = `el${i++}`;
-          this.addLine(`const ${current.name} = ${el};`);
+          current.line.elemDef = `const ${current.name} = ${el};`;
+          // this.addLine(`const ${current.name} = ${el};`);
         }
         el = `${current.name ? current.name : el}.${p}`;
         current = current[p];
         if (current.target && !current.name) {
           current.name = `el${i++}`;
-          this.addLine(`const ${current.name} = ${el};`);
+          current.line.elemDef = `const ${current.name} = ${el};`;
+          // this.addLine(`const ${current.name} = ${el};`);
         }
       }
       current.target = true;
     }
+    // console.warn('lines after', lines)
+    // console.warn(tree);
     for (let line of lines) {
-      const { path, inserter } = line;
+      const { path, inserter, elemDef } = line;
+      if (elemDef) {
+        this.addLine(elemDef);
+      }
       let current: any = tree;
       let el = `this`;
       for (let p of path.slice()) {
--- tests/compiler/misc.test.ts
+++ tests/compiler/misc.test.ts
@@ -68,12 +68,38 @@ describe("misc", () => {
 
   test("complex template", () => {
     const template = `
-      <div class="batch_header">
-        <a t-attf-href="/runbot/batch/{{batch.id}}" t-attf-class="badge badge-{{batch.has_warning ? 'warning' : 'light'}}" title="View Batch">
-            <t t-esc="batch.formated_age"/>
-            <i class="fa fa-exclamation-triangle" t-if="batch.has_warning"/>
-            <i class="arrow fa fa-window-maximize"/>
-        </a>
+      <div t-attf-class="batch_tile {{options.more? 'more' : 'nomore'}}">
+        <div t-attf-class="card bg-{{klass}}-light">
+          <div class="batch_header">
+              <a t-attf-href="/runbot/batch/{{batch.id}}" t-attf-class="badge badge-{{batch.has_warning ? 'warning' : 'light'}}" title="View Batch">
+                  <t t-esc="batch.formated_age"/>
+                  <i class="fa fa-exclamation-triangle" t-if="batch.has_warning"/>
+                  <i class="arrow fa fa-window-maximize"/>
+              </a>
+          </div>
+          <t t-if="batch.state=='preparing'">
+              <span><i class="fa fa-cog fa-spin fa-fw"/> preparing</span>
+          </t>
+          <div class="batch_slots">
+              <t t-foreach="batch.slot_ids.filter(slot => slot.build_id.id and !slot.trigger_id.manual and (options.trigger_display[slot.trigger_id.id]))" t-as="slot" t-key="slot.id">
+                  <SlotButton class="slot_container" slot="slot"/>
+              </t>
+              <div class="slot_filler" t-foreach="[1, 2, 3, 4]" t-as="x" t-key="x"/>
+          </div>
+          <div class="batch_commits">
+              <div t-foreach="commit_links" t-as="commit_link" class="one_line" t-key="commit_link.id">
+                  <a t-attf-href="/runbot/commit/{{commit_link.commit_id}}" t-attf-class="badge badge-light batch_commit match_type_{{commit_link.match_type}}">
+                  <i class="fa fa-fw fa-hashtag" t-if="commit_link.match_type == 'new'" title="This commit is a new head"/>
+                  <i class="fa fa-fw fa-link" t-if="commit_link.match_type == 'head'" title="This commit is an existing head from bundle branches"/>
+                  <i class="fa fa-fw fa-code-fork" t-if="commit_link.match_type == 'base_match'" title="This commit is matched from a base batch with matching merge_base"/>
+                  <i class="fa fa-fw fa-clock-o" t-if="commit_link.match_type == 'base_head'" title="This commit is the head of a base branch"/>
+                  <t t-esc="commit_link.commit_dname"/>
+                  </a>
+                  <a t-att-href="'https://%s/commit/%s' % (commit_link.commit_remote_url, commit_link.commit_name)" class="badge badge-light" title="View Commit on Github"><i class="fa fa-github"/></a>
+                  <span t-esc="commit_link.commit_subject"/>
+              </div>
+          </div>
+        </div>
       </div>`;
     
       snapshotTemplateCode(template);
